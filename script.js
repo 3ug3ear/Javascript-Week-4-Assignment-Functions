4ff47a189af0ece6aaf29e5c63ca1d0ec86bee9b
Brandon Bravo <116048121+3ug3ear@users.noreply.github.com>
2022-11-15 22:49:21 -0800

    finished search string

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ console.log(circumference(4))
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
 
+function char_count(str, letter) 
+{
+ let letter_Count = 0;
+ for (let position = 0; position < str.length; position++) 
+ {
+    if (str.charAt(position) == letter) 
+      {
+      letter_Count += 1;
+      }
+  }
+  return letter_Count;
+}
+
+console.log(char_count('mississippi', 's'));
